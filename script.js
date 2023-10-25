6109703d1a343796016155177c6033a0a8be73a5
Codsss <Jchrstn0401@gmail.com>
2023-10-25 16:37:12 +0800

    Update html, css, mediaqueries, script and assets
    Finished contents for ROLES.

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ toggleSkillElements.forEach((toggleSkill) => {
         }
     });
 });
+
+
+const btns = document.querySelectorAll(".navigation__col");
+const slideVideo = document.querySelectorAll(".slider__video");
+const slides = document.querySelectorAll(".slider__right__image");
+
+let sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  slideVideo.forEach((slideVideo) => {
+    slideVideo.classList.remove("active");
+  });
+
+  slides.forEach((slides) => {
+    slides.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slideVideo[manual].classList.add("active");
+  slides[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
