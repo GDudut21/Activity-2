c23d08322d72eadd1420cd8726b93578b167023c
Albean12 <alvinguatato9@gmail.com>
2023-10-29 14:34:50 +0800

    Minimal update in Champions Section and also in maps
    Note: Skill hover description remove , also color change in hover in maps section
    
    PS: STILL OPEN FOR CHANGES

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Get all the "Show Skill" elements
-const toggleSkillElements = document.querySelectorAll(".toggle-skill");
-
-// Add click event listeners to each "Show Skill" element
-toggleSkillElements.forEach((toggleSkill) => {
-    toggleSkill.addEventListener("click", () => {
-        // Find the associated skill description
-        const skillDescription = toggleSkill.nextElementSibling;
-
-        // Toggle the display of the skill description
-        if (skillDescription.style.display === "block") {
-            skillDescription.style.display = "none";
-        } else {
-            skillDescription.style.display = "block";
-        }
-    });
-});
 
 
 const btns = document.querySelectorAll(".navigation__col");
